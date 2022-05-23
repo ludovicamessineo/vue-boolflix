9c04fc8a41c4237f95a436787df27f9a3c34662f
ludovicamessineo <ludovica.messineo9@gmail.com>
2022-05-23 15:59:58 +0200

    add header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <input type="text" placeholder="Inserisci un titolo">
+    <button>Cerca</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+ .header {
+     height: 80px;
+     background-color: #141414;
+     display: flex;
+     justify-content: flex-end;
+     align-items: center;
+     padding-right: 2rem;
+
+     input {
+         width: 15%;
+         padding: .2rem;
+     }
+
+     button {
+         padding: .2rem .5rem;
+         margin-left: .2rem;
+     }
+ }
+</style>
